5a2916aee5d63897656831f564dc8401dea19e1a
zhongxin <490336534@qq.com>
2019-10-31 22:34:12 +0800

    注册的时候校验账号密码

--- user/urls.py
+++ user/urls.py
@@ -6,11 +6,15 @@
 @File: urls.py
 @Email: 490336534@qq.com
 """
-from django.urls import path
+from django.urls import path, re_path
 from rest_framework_jwt.views import obtain_jwt_token
 from . import views
 
 urlpatterns = [
     path('login/', obtain_jwt_token),
     path('register/', views.RegisterView.as_view()),
+    re_path(r'^(?P<username>\w{6,20})/count/$',
+            views.UsernameValidateView.as_view(), name='check_username'),
+    re_path(r'^(?P<email>[A-Za-z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z0-9_-]+)/count/$',
+            views.EmailValidateView.as_view(), name='check_email'),
 ]
